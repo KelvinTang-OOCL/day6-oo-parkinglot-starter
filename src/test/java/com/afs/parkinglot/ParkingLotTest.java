e368d0786e5695283d300ff025841fad5a8c32a3
KelvinTang <tangke3@oocl.com>
2025-09-08 17:40:05 +0800

    feat: add not full ParkingLot and Car park test

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -2,17 +2,20 @@ package com.afs.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ParkingLotTest {
     @Test
-    void should_return_when__given_() {
+    void should_return_ParkingTicket_when_park_given_not_full_ParkingLot_and_Car() throws NoAvailablePositionException {
         //Given
-
+        ParkingLot parkingLot = new ParkingLot(10);
+        Car car = new Car();
 
         //When
+        ParkingTicket parkingTicket = parkingLot.park(car);
 
 
         //Then
-
-
+        assertNotNull(parkingTicket);
     }
 }
